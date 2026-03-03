e8a0c3315d51db82986163ecfe80e33e6c966585
Carlos P. <dawcarlospereira@gmail.com>
2026-03-03 19:54:50 +0100

    Mejora de rendimiento

--- js/particles.js
+++ js/particles.js
@@ -1,25 +1,39 @@
 tsParticles.load("tsparticles", {
-      background: { color: { value: "#0B132B" } },
-      fpsLimit: 60,
-      interactivity: {
-        events: {
-          onClick: { enable: true, mode: "push" },
-          onHover: { enable: true, mode: "repulse" },
-          resize: true
-        },
-        modes: {
-          push: { quantity: 4 },
-          repulse: { distance: 100, duration: 0.4 }
-        }
-      },
-      particles: {
-        color: { value: "#1aff8c" },
-        links: { color: "#1aff8c", distance: 150, enable: true, opacity: 0.5, width: 1 },
-        move: { enable: true, speed: 2, outModes: "bounce" },
-        number: { value: 80, density: { enable: true, area: 800 }, limit: 50 },
-        opacity: { value: 0.5 },
-        shape: { type: "circle" },
-        size: { value: { min: 1, max: 5 } }
-      },
-      detectRetina: true
-    });
+  fpsLimit: 30, 
+  fullScreen: { enable: true, zIndex: -1 },
+  interactivity: {
+    events: {
+      onClick: { enable: true, mode: "push" },
+      onHover: { enable: true, mode: "repulse" },
+      resize: true
+    },
+    modes: {
+      push: { quantity: 3 }, 
+      repulse: { distance: 100, duration: 0.4 }
+    }
+  },
+  particles: {
+    number: { 
+      value: 40,
+      limit: 80, 
+      density: { enable: true, area: 1000 } 
+    },
+    color: { value: "#1aff8c" },
+    links: { 
+      enable: true, 
+      distance: 150, 
+      color: "#1aff8c", 
+      opacity: 0.2, 
+      width: 1 
+    },
+    move: { 
+      enable: true, 
+      speed: 1, 
+      outModes: "out"
+    },
+    size: { value: { min: 1, max: 3 } },
+    opacity: { value: 0.4 }
+  },
+  pauseOnBlur: true, 
+  detectRetina: false 
+});
